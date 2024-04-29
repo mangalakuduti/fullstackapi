d0dc03f880f75f52bb8ca16b470a91db3641cb3a
Manas Angalakuduti <manasangalakuduti@Manass-MacBook-Pro.local>
2024-04-29 11:01:03 -0500

    initial commit

---
+++ src/test/java/dev/manas/fullstackapi/FullstackapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manas.fullstackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

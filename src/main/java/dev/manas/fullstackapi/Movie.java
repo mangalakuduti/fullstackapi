05a9fd8c7cd454d92ed6fe201e9b3aa2f183b620
Manas Angalakuduti <manasangalakuduti@gmail.com>
2024-05-13 21:27:29 -0500

    Movie, Review Class + Controller

---
+++ src/main/java/dev/manas/fullstackapi/Movie.java
@@ -0,0 +1,29 @@
+package dev.manas.fullstackapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // Creates getters and setters
+@AllArgsConstructor // Creates constructor for all private fields
+@NoArgsConstructor // Creates constructor for no parameters
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // Establishes relations between documents
+    private List<Review> reviewIds;
+}
